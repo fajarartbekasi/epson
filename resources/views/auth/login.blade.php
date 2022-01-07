d0761ab8daa598c4f80e0d2bfba3677bc70600c7
fajarartbekasi <fajarbekasieditting@gmail.com>
2022-01-07 13:39:44 +0700

    remove forgotpaswword

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,13 +40,6 @@
                                         {{ __('Remember Me') }}
                                     </label>
                                 </div>
-                                <div class="">
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
-                                </div>
                             </div>
                         </div>
 
