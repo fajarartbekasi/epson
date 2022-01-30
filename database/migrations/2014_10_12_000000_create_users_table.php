256eb5dc15fa0b83aee37d4eff80ac09fc53e460
fajarartbekasi <fajarbekasieditting@gmail.com>
2022-01-30 19:52:38 +0700

    change struktur password & token

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->longText('address');
             $table->string('phone', 15);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password',60);
             $table->rememberToken();
             $table->timestamps();
         });
